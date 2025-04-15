6fcb4d7ff765e7bd83a5174170fb3e34febaef5a
dxzielinski <dominik.zielinski2002@gmail.com>
2025-04-15 14:09:19 +0200

    update workflow

--- firebase.py
+++ firebase.py
@@ -1,8 +1,6 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate(
-    "steadfast-wares-453916-n8-firebase-adminsdk-fbsvc-168d7e9cf7.json"
-)
+cred = credentials.Certificate("cert.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client(database_id="proj")
