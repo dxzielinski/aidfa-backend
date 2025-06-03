7467a0827e86a7fb6213244d91f700ce4a35aa57
dxzielinski <dominik.zielinski2002@gmail.com>
2025-06-03 13:29:20 +0200

    cert

--- firebase.py
+++ firebase.py
@@ -3,6 +3,9 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 cert = os.getenv("BACKEND_SERVICE_ACCOUNT")
-cred = credentials.Certificate(cert)
+with open("cert.json", "w") as f:
+    f.write(cert)
+
+cred = credentials.Certificate("cert.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client(database_id="proj")
